d7d432fda46dcf4f095b2edd816c6a46725715c2
Tom Siatka <tom.siatka@laposte.net>
2021-01-14 16:31:35 +0100

    modif cours 14/01

--- src/main.js
+++ src/main.js
@@ -1,3 +1,35 @@
+//scrollspy
+
+document.addEventListener('DOMContentLoaded', function(){ 
+  
+  const sections = document.querySelectorAll(".section");
+  const menu_links = document.querySelectorAll(".nav li");
+  
+  //fonction d'ajout/retirer classe
+  const makeActive = (link) => menu_links[link].classList.add("active");
+  const removeActive = (link) => menu_links[link].classList.remove("active");
+  const removeAllActive = () => [...Array(sections.length).keys()].forEach((link) => removeActive(link));
+
+  //marge scrollspy
+  const sectionMargin = 200;
+
+  let currentActive = 0;
+
+  window.addEventListener("scroll", () => {
+    
+    //trouve section courante
+    const current = sections.length - [...sections].reverse().findIndex((section) => window.scrollY >= section.offsetTop - sectionMargin ) - 1
+
+    // supprime classe active sur toutes les sections 
+    // ajoute classe active sur section active
+    if (current !== currentActive) {
+      removeAllActive();
+      currentActive = current;
+      makeActive(current);
+    }
+  });
+}, false);
+
 /* let elems = document.querySelectorAll('a')
 for(var i = 0; i<elems.length; i++) {
   elems[i].addEventListener('click', function(){
